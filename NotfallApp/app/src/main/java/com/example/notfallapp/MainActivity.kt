8829d93259724aee1488d5c96b9621a60d74fcb7
BenHufschmied <hacksteb@edu.htl-villach.at>
2020-07-07 14:04:25 +0200

    implement first version of menubar

--- NotfallApp/app/src/main/java/com/example/notfallapp/MainActivity.kt
+++ NotfallApp/app/src/main/java/com/example/notfallapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.notfallapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var toolbar = findViewById<Toolbar>(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean{
+        menuInflater.inflate(R.menu.menubar, menu)
+        return true
     }
 }
