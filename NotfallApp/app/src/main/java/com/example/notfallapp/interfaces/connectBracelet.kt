ce674543dfb07f963f9ce567f591ac3b1da37f9a
LukasMurnig <lukas.murnig@gmail.com>
2020-07-27 08:58:40 +0200

    Code Refactoring of the new Class ServerUser.kt. Interface connectBracelet.kt with some changes and AddBraceletActivity.kt uses the interfaces now.

--- NotfallApp/app/src/main/java/com/example/notfallapp/interfaces/connectBracelet.kt
+++ NotfallApp/app/src/main/java/com/example/notfallapp/interfaces/connectBracelet.kt
@@ -1,32 +1,170 @@
 package com.example.notfallapp.interfaces
 
 import android.bluetooth.*
-import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.example.notfallapp.MainActivity
 import com.example.notfallapp.bll.ReadWriteCharacteristic
 import com.example.notfallapp.connectBracelet.AddBraceletActivity
 import com.example.notfallapp.connectBracelet.Constants
 import com.example.notfallapp.connectBracelet.ProcessQueueExecutor
+import com.example.notfallapp.server.ServerApi.Companion.TAG
 import com.example.notfallapp.service.ServiceCallAlarm
 
 interface connectBracelet {
 
     companion object{
         private var batteryState: String = " "
+        private var context: Context? = null
+        var gattBluetooth: BluetoothGatt? = null
+        var process: ProcessQueueExecutor = ProcessQueueExecutor()
+        var mGattCallbacks: BluetoothGattCallback = object : BluetoothGattCallback(){}
     }
-
-    var bluetoothGatt: BluetoothGatt?
-    var process: ProcessQueueExecutor
 
     fun connect(context: Context, device: BluetoothDevice){
         process = ProcessQueueExecutor()
-        bluetoothGatt = device.connectGatt(context, false, gattCallback)
+        //To execute the read and write operation in a queue.
+        if (!process.isAlive()) {
+            process.start()
+        }
+        connectBracelet.context = context
+        gattBluetooth = device.connectGatt(context, false, mGattCallbacks)
+        mGattCallbacks = object : BluetoothGattCallback() {
+            override fun onConnectionStateChange(
+                gatt: BluetoothGatt,
+                status: Int,
+                newState: Int
+            ) {
+                val device = gatt.device
+                val deviceAddress = device.address
+                try {
+                    when (newState) {
+                        BluetoothProfile.STATE_CONNECTED ->                    //start service discovery
+                            gatt.discoverServices()
+                        BluetoothProfile.STATE_DISCONNECTED -> {
+                            try {
+                                gatt.disconnect()
+                                gatt.close()
+                            } catch (e: Exception) {
+                                Log.e(TAG, e.message)
+                            }
+                        }
+                        else -> {
+                        }
+                    }
+                } catch (e: NullPointerException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+                val device = gatt.device
+                val deviceAddress = device.address
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    for (service in gatt.services) {
+                        if (service == null || service.uuid == null) {
+                            continue
+                        }
+                        if (Constants.SERVICE_BATTERY_LEVEL.equals(service.uuid)) {
+                            //Read the device battery percentage
+                            readCharacteristic(
+                                gatt,
+                                service.getCharacteristic(Constants.CHAR_BATTERY_LEVEL)
+                            )
+                        }
+                    }
+                } else {
+                    // Service discovery failed close and disconnect the GATT object of the device.
+                    gatt.disconnect()
+                    gatt.close()
+                }
+            }
+
+            // CallBack when the response available for registered the notification( Battery Status, Fall Detect, Key Press)
+            override fun onCharacteristicChanged(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic
+            ) {
+
+//			Intent i = new Intent(Intent.ACTION_CAMERA_BUTTON);
+                val keyValue =
+                    characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0).toString()
+                if (characteristic.uuid == Constants.CHAR_DETECTION_NOTIFY) {
+                    if (keyValue == "1") {
+                        var intent = Intent(Companion.context, ServiceCallAlarm::class.java)
+                        Companion.context?.startActivity(intent)
+                    } else if (keyValue == "0") {
+                        var intent = Intent(Companion.context, ServiceCallAlarm::class.java)
+                        Companion.context?.startActivity(intent)
+                    } else if (keyValue == "3") {
+                        println("2-10 second press release")
+                    } else if (keyValue == "4") {
+                        println("fallevent")
+                    } else if (keyValue == "5") {
+                        println("high g event")
+                    } else {
+                        println(keyValue)
+                    }
+                }
+            }
+
+            // Callback when the response available for Read Characteristic Request
+            override fun onCharacteristicRead(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic,
+                status: Int
+            ) {
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    // Display received battery value.
+                    if (Constants.CHAR_BATTERY_LEVEL.equals(characteristic.uuid)) {
+                        batteryState =
+                            characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0)
+                                .toString()
+                    } else {
+                        Log.i(
+                            TAG,
+                            "received characteristic read:" + characteristic.uuid.toString()
+                        )
+                    }
+                }
+            }
+
+            // Callback when the response available for Write Characteristic Request
+            override fun onCharacteristicWrite(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic,
+                status: Int
+            ) {
+            }
+
+            // Callback when the response available for Read Descriptor Request
+            override fun onDescriptorRead(
+                gatt: BluetoothGatt,
+                descriptor: BluetoothGattDescriptor,
+                status: Int
+            ) {
+                Log.i(TAG, "received descriptor read:" + descriptor.uuid.toString())
+            }
+
+            // Callback when the response available for Write Descriptor Request
+            override fun onDescriptorWrite(
+                gatt: BluetoothGatt,
+                descriptor: BluetoothGattDescriptor,
+                status: Int
+            ) {
+            }
+
+            override fun onReadRemoteRssi(gatt: BluetoothGatt, rssi: Int, status: Int) {
+                if (status == BluetoothGatt.GATT_SUCCESS) {
+                    val rssiValue = Integer.toString(rssi)
+                }
+            }
+        }
+        var intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
     }
 
-    // Various callback methods defined by the BLE API.
-    val gattCallback: BluetoothGattCallback
 
     //Read the value of the BLE device
     fun readCharacteristic(
@@ -66,9 +204,9 @@ interface connectBracelet {
     }
 
     fun close() {
-        bluetoothGatt?.disconnect()
-        bluetoothGatt?.close()
-        bluetoothGatt = null
+        gattBluetooth?.disconnect()
+        gattBluetooth?.close()
+        gattBluetooth = null
         process.interrupt()
     }
 }
--- NotfallApp/app/src/main/java/com/example/notfallapp/server/ServerUser.kt
+++ NotfallApp/app/src/main/java/com/example/notfallapp/server/ServerUser.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import com.android.volley.AuthFailureError
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.example.notfallapp.R
 import com.example.notfallapp.bll.User
 import org.json.JSONObject
 import java.util.*
@@ -16,49 +17,54 @@ class ServerUser {
         ServerApi.controlToken()
 
         val jsonObjectRequest: JsonObjectRequest = object : JsonObjectRequest(
-            Method.GET, ServerApi.serverAPIURL+"/users/me", null,
+            Method.GET, ServerApi.serverAPIURL+context.getString(R.string.UserMe), null,
             Response.Listener<JSONObject> { response ->
-                Log.e(ServerApi.TAG, "response: $response")
+                Log.e(ServerApi.TAG,
+                      String.format(context.getString(R.string.responseMessage), response.toString()))
                 try {
-                    val isSuccess = response.getBoolean("isSuccess")
-                    val code = response.getInt("code")
-                    val message = response.getString("message")
-                    if (response.has("data")) {
+                    val isSuccess = response.getBoolean(context.getString(R.string.isSuccess))
+                    val code = response.getInt(context.getString(R.string.code))
+                    val message = response.getString(context.getString(R.string.messageServer))
+                    if (response.has(context.getString(R.string.dataServer))) {
                         // array of json?
-                        val data = response.getJSONObject("data")
+                        val data = response.getJSONObject(context.getString(R.string.dataServer))
                         val user = User(
-                            data.get("ID") as UUID,
-                            data.get("ForeignId") as String?,
-                            data.get("Title") as String?,
-                            data.getString("Forename"),
-                            data.getString("Surename"),
-                            data.getString("Username"),
-                            data.getBoolean("Active"),
-                            data.getString("Role"),
-                            data.getBoolean("Gender"),
-                            data.getBoolean("PhotoSet"),
-                            data.get("BirthDay") as Date?,
-                            data.get("EmailAddress") as String?,
-                            data.get("PhoneFixed") as String?,
-                            data.getInt("OrgUnit"),
-                            data.get("Language") as String?,
-                            data.get("Timezone") as String?
+                            data.get(context.getString(R.string.id)) as UUID,
+                            data.get(context.getString(R.string.foreignId)) as String?,
+                            data.get(context.getString(R.string.title)) as String?,
+                            data.getString(context.getString(R.string.Forename)),
+                            data.getString(context.getString(R.string.Surename)),
+                            data.getString(context.getString(R.string.usernameServer)),
+                            data.getBoolean(context.getString(R.string.activeServer)),
+                            data.getString(context.getString(R.string.roleServer)),
+                            data.getBoolean(context.getString(R.string.genderServer)),
+                            data.getBoolean(context.getString(R.string.photoSetServer)),
+                            data.get(context.getString(R.string.photoSetServer)) as Date?,
+                            data.get(context.getString(R.string.emailAddressServer)) as String?,
+                            data.get(context.getString(R.string.phoneFixedServer)) as String?,
+                            data.getInt(context.getString(R.string.OrgUnitSever)),
+                            data.get(context.getString(R.string.LanguageServer)) as String?,
+                            data.get(context.getString(R.string.TimezoneServer)) as String?
                         )
 
                     }
                 } catch (e: Exception) { // caught while parsing the response
-                    Log.e(ServerApi.TAG, "problem occurred")
+                    Log.e(ServerApi.TAG,
+                          String.format(context.getString(R.string.ErrorServerUserMessage),
+                                        context.getString(R.string.ServerUser), e.toString()))
                     e.printStackTrace()
                 }
             }, Response.ErrorListener { error ->
                 val resErrorBody = JSONObject(String(error.networkResponse.data))
-                Log.e(ServerApi.TAG, "problem occurred, volley error: " + error.networkResponse.statusCode + " " + resErrorBody.get("Error"))
+                Log.e(ServerApi.TAG,
+                    String.format(context.getString(R.string.ErrorServerUserResponseMessage),
+                        error.networkResponse.statusCode , resErrorBody.get("Error")))
             }) {
             @Throws(AuthFailureError::class)
             override fun getHeaders(): Map<String, String> {
                 var params: MutableMap<String, String>? = super.getHeaders()
                 if (params == null) params = HashMap()
-                params["Authorization"] = ServerApi.accessToken.toString()
+                params[context.getString(R.string.authorizationServer)] = ServerApi.accessToken.toString()
                 //..add other headers
                 return params
             }
