bd8a867e264bd3f45023bf66d48b2bca160698a3
BenHufschmied <hacksteb@edu.htl-villach.at>
2020-07-27 13:15:29 +0200

    add device dao

--- NotfallApp/app/src/main/java/com/example/notfallapp/bll/Device.kt
+++ NotfallApp/app/src/main/java/com/example/notfallapp/bll/Device.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Devices")
-class Device constructor(macAddress: String) {
+data class Device constructor(val macAddress: String) {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     var id: Int = 0
