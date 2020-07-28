7764ac58d95d62a7403d1a78c9be6b41e04fefea
LukasMurnig <lukas.murnig@gmail.com>
2020-07-28 15:54:51 +0200

    Layout changes regarding the add Button

--- NotfallApp/app/src/main/java/com/example/notfallapp/receiver/BootUpReceiver.kt
+++ NotfallApp/app/src/main/java/com/example/notfallapp/receiver/BootUpReceiver.kt
@@ -4,23 +4,30 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.preference.PreferenceManager
+import android.widget.Toast
 import com.example.notfallapp.MainActivity
+import com.example.notfallapp.interfaces.IConnectBracelet
+import com.example.notfallapp.service.ServiceCallAlarm
 
 class BootUpReceiver : BroadcastReceiver() {
 
     // should open MainActivity, when device is finished with boot up
     override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "HELLO BOOST", Toast.LENGTH_LONG).show()
         if (context != null && intent !=null){
             val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-
-            if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
-                // ?
+                Toast.makeText(context, "HELLO WE Boot UP", Toast.LENGTH_LONG).show()
+                var intent = Intent(context, ServiceCallAlarm::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                context?.startService(intent)
                 if(prefs.getBoolean("switch", true)){
-                    val i = Intent(context, MainActivity::class.java)
+                    var intent = Intent(context, ServiceCallAlarm::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    context?.startService(intent)
+                    /*val i = Intent(context, MainActivity::class.java)
                     i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    context.startActivity(i)
+                    context.startActivity(i)*/
                 }
-            }
         }
     }
 
