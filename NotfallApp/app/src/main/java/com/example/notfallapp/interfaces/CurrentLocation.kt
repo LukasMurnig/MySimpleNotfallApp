7e254573223ee4d6cf197c549aba53c651d68f49
LukasMurnig <lukas.murnig@gmail.com>
2020-09-08 10:30:13 +0200

    Change for The gps to get Location

--- NotfallApp/app/src/main/java/com/example/notfallapp/interfaces/CurrentLocation.kt
+++ NotfallApp/app/src/main/java/com/example/notfallapp/interfaces/CurrentLocation.kt
@@ -10,6 +10,7 @@ import android.location.LocationManager
 import android.os.AsyncTask
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import java.util.concurrent.Executors
 
 /**
  * Class where we get our current Location back.
@@ -32,10 +33,10 @@ class CurrentLocation {
 
                 if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                     gps = true
-                    getLastKnownLocation()
+                    searchForLocation()
                 } else if (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
                     gps = false
-                    getLastKnownLocation()
+                    searchForLocation()
                 }
 
             return currentLocation
@@ -57,66 +58,66 @@ class CurrentLocation {
 
         }
 
+        fun searchForLocation(){
+            val executor = Executors.newFixedThreadPool(5)
+            val worker = Runnable { getLastKnownLocation() }
+            executor.execute(worker)
+            executor.shutdown()
+        }
+
         /**
          * Function who set the currentLocation to a variable which will be returned in getCurrentLocation
          */
         fun getLastKnownLocation() {
-            class FindLocation : AsyncTask<Unit, Unit, Unit>() {
+            if (context?.let {
+                    ActivityCompat.checkSelfPermission(
+                        it,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                } != PackageManager.PERMISSION_GRANTED && context?.let {
+                    ActivityCompat.checkSelfPermission(
+                        it,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                } != PackageManager.PERMISSION_GRANTED
+            ) {
+                //return null
+            }
 
-                override fun doInBackground(vararg p0: Unit?) {
-                    if (context?.let {
-                            ActivityCompat.checkSelfPermission(
-                                it,
-                                Manifest.permission.ACCESS_FINE_LOCATION
-                            )
-                        } != PackageManager.PERMISSION_GRANTED && context?.let {
-                            ActivityCompat.checkSelfPermission(
-                                it,
-                                Manifest.permission.ACCESS_COARSE_LOCATION
-                            )
-                        } != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        //return null
-                    }
+            val permissionGranted: Boolean = context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it, Manifest.permission.ACCESS_FINE_LOCATION)
+            } == PackageManager.PERMISSION_GRANTED
 
-                    val permissionGranted: Boolean = context?.let {
-                        ActivityCompat.checkSelfPermission(
-                            it, Manifest.permission.ACCESS_FINE_LOCATION)
-                    } == PackageManager.PERMISSION_GRANTED
+            if (!permissionGranted){
+                try {
+                    ActivityCompat.requestPermissions(context as Activity, Array(3) { Manifest.permission.ACCESS_FINE_LOCATION }, 200)
+                } catch (ex: RuntimeException) {
+                    println("Error")
+                }
+            } else {
+                if (gps) {
+                    locationManager?.removeUpdates(this@Companion)
+                    locationManager?.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0,
+                        200.0F,
+                        this@Companion
+                    )
+                    currentLocation =
+                        locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                } else {
 
-                    if (!permissionGranted){
-                        try {
-                        ActivityCompat.requestPermissions(context as Activity, Array(3) { Manifest.permission.ACCESS_FINE_LOCATION }, 200)
-                        } catch (ex: RuntimeException) {
-                            println("Error")
-                        }
-                    } else {
-                        if (gps) {
-                            locationManager?.removeUpdates(this@Companion)
-                            locationManager?.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER,
-                                0,
-                                200.0F,
-                                this@Companion
-                            )
-                            currentLocation =
-                                locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                        } else {
-
-                            locationManager?.requestLocationUpdates(
-                                LocationManager.NETWORK_PROVIDER,
-                                0,
-                                200.0F,
-                                this@Companion
-                            )
-                            currentLocation =
-                                locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-                        }
-                    }
+                    locationManager?.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        0,
+                        200.0F,
+                        this@Companion
+                    )
+                    currentLocation =
+                        locationManager?.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
                 }
             }
-                val gd = FindLocation()
-                gd.execute()
         }
     }
 }
